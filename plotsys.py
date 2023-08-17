9a655851d03df8ea24e65526397c6f9dc4eba0fc
lochie <lochie.denholm@carsales.com.au>
2023-08-17 17:37:12 +1000

    add lin sys

--- plotsys.py
+++ plotsys.py
@@ -2,31 +2,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-# Setup figure:
-fig =  plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-
-
-def parabola(x, y):
-    return x**2 +y**2
-
-def inverse_prabola(x, y):
-    return -1*(x**2 + y**2) - 100
-
-# Matplotlib needs a meshgrid to plot the functions
-x, y = np.arange(-25, 25, 1), np.arange(-25, 25, 1)
-X, Y = np.meshgrid(x, y)
-
-# Z = f(x,y)
-Z = parabola(X, Y)
-ZZ = inverse_prabola(X, Y)
-
-print('x and y lists', x, y)
-print('X mesh', X)
-
-ax.plot_surface(X, Y, Z, alpha=0.5)
-ax.plot_surface(X, Y, ZZ, alpha=0.5)
-
 # Let's render a system of linear equations:
 # So essentially we create intervals of reals (floating points)
 # then construct a mesh from those intervals seems to be: [a, b).
@@ -45,7 +20,9 @@ def plotSystem(ax):
     ax.plot_surface(X, Y, Z1, alpha=0.5)
     ax.plot_surface(X, Y, Z2, alpha=0.5)
 
-plotSystem(ax)
+# Setup figure:
+fig =  plt.figure()
+ax = fig.add_subplot(111, projection='3d')
 
-# To render plot:
+plotSystem(ax)
 plt.show()
