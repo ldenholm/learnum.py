215a188f7d062b3cdd421a38eabbfd713933de58
lochie <lochie.denholm@carsales.com.au>
2023-08-21 14:14:33 +1000

    add L inverse

--- problem25.py
+++ problem25.py
@@ -1,4 +1,5 @@
 import numpy as np
+import scipy.linalg as la
 
 K = np.array([[2, -1, 0, 0, 0, 0],
               [-1, 2, -1, 0, 0, 0],
@@ -16,3 +17,15 @@ print("IK:\n", IK)
 # Test commutativity
 KI = np.matmul(K, I)
 print("KI:\n", KI)
+
+# Decompose K = LU
+P, L, U = la.lu(K)
+print("Upper triangular of K:\n", U)
+print("Lower triangular of K:\n", L)
+
+# Question asks us to find a formula for constructing the
+# i,jth entry of L^-1. Note E^-1 = L and L^-1 = E.
+
+# We have L so let's compute it's inverse:
+E = np.linalg.inv(L)
+print('E:\n', E)
