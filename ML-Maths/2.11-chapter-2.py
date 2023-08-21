18fa2ec29e8614102747673c5bdd17ee163e7dbb
lochie <lochie.denholm@carsales.com.au>
2023-08-21 15:53:10 +1000

    add todo

--- ML-Maths/2.11-chapter-2.py
+++ ML-Maths/2.11-chapter-2.py
@@ -1,4 +1,6 @@
 import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 A = np.array([[1, 1, 2],
               [1, 2, -1],
@@ -7,3 +9,5 @@ b = np.array([1, -2, 5])
 #Ax = b
 x = np.linalg.solve(A, b)
 print(x)
+
+# Todo: Import matplotlib and plot.
