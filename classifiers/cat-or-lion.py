94bf2883a886b11194b25fbf1de7a685e94584c7
lochie <lochie.denholm@carsales.com.au>
2023-08-24 17:57:49 +1000

    dl cats and lions

--- classifiers/cat-or-lion.py
+++ classifiers/cat-or-lion.py
@@ -1,11 +1,22 @@
-# First we need to download images of cats and lions
-# with varying lighting conditions.
-
+import ssl
+import sys
 from time import sleep
-import DuckDuckGoImages as ddg
 from duckduckgo_search import ddg_images
 from fastcore.all import *
 from fastdownload import download_url
+
+# SSL Setup
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    # Legacy Python that doesn't verify HTTPS certificates by default
+    pass
+else:
+    # Handle target environment that doesn't support HTTPS verification
+    ssl._create_default_https_context = _create_unverified_https_context
+
+# First we need to download images of cats and lions
+# with varying lighting conditions.
 
 def search_images(term, max_imgs=30):
     print(f"Searching for '{term}'")
@@ -14,14 +25,23 @@ def search_images(term, max_imgs=30):
 # Now we will search for some lions and cat images:
 # Let's compile a list of URL's then we can feed those URL's into the downloader fn:
 
-urls = search_images('photos of lion', max_imgs=10)
-for url in urls:
-    print(url)
-
 def download_images(term: str, urls: list):
     for idx, img in enumerate(urls):
-        download_url(img, f"{term}-{idx}")
+        download_url(img, f"./{term}/{term}-{idx}.png")
         # Minor sleep so as to not overload the server:
         sleep(2)
 
-download_images('lion', urls)
+def run_downloader(mode: str):
+    match mode:
+        case "lions":
+            download_images('lions', search_images('photos of lion', max_imgs=10))
+        case "cats":
+            download_images('cats', search_images('pitcures of majestic of cats', max_imgs=10))
+        case "both":
+            download_images('lions', search_images('photos of lion', max_imgs=10))
+            download_images('cats', search_images('pitcures of majestic of cats', max_imgs=10))
+        case _:
+            print("Skipping download")
+
+if (len(sys.argv)>1):
+    run_downloader(sys.argv[1])
