b919e28af1e3f48373d6913545bfa273ceba6059
lochie <lochie.denholm@carsales.com.au>
2023-08-24 19:03:03 +1000

    todo fix dls

--- classifiers/cat-or-lion.py
+++ classifiers/cat-or-lion.py
@@ -4,6 +4,8 @@ from time import sleep
 from duckduckgo_search import ddg_images
 from fastcore.all import *
 from fastdownload import download_url
+from fastai.data.all import *
+from fastai.vision.all import *
 
 # SSL Setup
 try:
@@ -18,6 +20,8 @@ else:
 # First we need to download images of cats and lions
 # with varying lighting conditions.
 
+PATH = Path('./cat-or-lion')
+
 def search_images(term, max_imgs=30):
     print(f"Searching for '{term}'")
     return L(ddg_images(term, max_results=max_imgs)).itemgot('image')
@@ -27,7 +31,7 @@ def search_images(term, max_imgs=30):
 
 def download_images(term: str, urls: list):
     for idx, img in enumerate(urls):
-        download_url(img, f"./{term}/{term}-{idx}.png")
+        download_url(img, f"./cat-or-lion/{term}-{idx}.png")
         # Minor sleep so as to not overload the server:
         sleep(2)
 
@@ -42,6 +46,15 @@ def run_downloader(mode: str):
             download_images('cats', search_images('pitcures of majestic of cats', max_imgs=10))
         case _:
             print("Skipping download")
+
+dls = DataBlock(
+    blocks=(ImageBlock, CategoryBlock),
+    get_items=get_image_files(PATH),
+    splitter=RandomSplitter(valid_pct=0.2, seed=42),
+                            get_y=parent_label,
+                            item_tfms=[Resize(192, method='squish')]
+                            ).dataloaders(PATH, bs=32)
+dls.show_batch(max_n=6)
 
 if (len(sys.argv)>1):
     run_downloader(sys.argv[1])
