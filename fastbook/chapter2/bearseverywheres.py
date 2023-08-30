86550766f64227f9239815bbf31b31b718858a60
lochie <lochie.denholm@carsales.com.au>
2023-08-30 15:00:07 +1000

    Add bear downloader

---
+++ fastbook/chapter2/bearseverywheres.py
@@ -0,0 +1,34 @@
+import sys
+import fastbook
+from fastbook import *
+from fastai.vision.widgets import *
+
+def main():
+    fastbook.setup_book()
+    key = sys.argv[1]
+    bear_types = 'grizzly','black','teddy'
+    path = Path('bears')
+    for o in bear_types:
+        dest = (path/o)
+        dest.mkdir(exist_ok=True)
+        results = search_images_bing(key, f'{o} bear')
+        download_images(dest, urls=results.attrgot('contentUrl'))
+
+if __name__ == "__main__":
+    main()
+
+
+#-------------------------------------------------------------------------------------
+#results = search_images_bing(key, 'grizzly bear')
+#ims = results.attrgot('contentUrl')
+#print('first entry in ims', ims[0])
+#
+
+# if not path.exists():
+#     path.mkdir()
+#     for o in bear_types:
+#         dest = (path/o)
+#         dest.mkdir(exist_ok=True)
+#         results = search_images_bing(key, f'{o} bear')
+#         download_images(dest, urls=results.attrgot('contentUrl'))
+#-------------------------------------------------------------------------------------
