23a28a33e36a20b7d934080afce92bec80f16660
lochie <disrute@gmail.com>
2023-09-11 11:32:18 +1000

    fix formatting

--- intro-to-computational-thinking/problemset1/ps1b.py
+++ intro-to-computational-thinking/problemset1/ps1b.py
@@ -23,17 +23,21 @@ def dp_make_weight(egg_weights, target_weight, memo = {}):
     Returns: int, smallest number of eggs needed to make target weight
     """
     # TODO: Your code here
+    """
     I'm just going to rewrite the problem statement because honestly the text above ^ is confusing.
     We must return the smallest set of eggs whose weight sum equals the total weight given. 
    
-    How can we tackle this problem and model it mathematically? The question is asking us to find a solution which    optimizes for the least number of eggs required to meet the weight constraint. I am considering representing 
+    How can we tackle this problem and model it mathematically? The question is asking us to find a solution which
+    optimizes for the least number of eggs required to meet the weight constraint. I am considering representing 
     the problem with a binary search tree but examples I have seen use a recursive function instead.
 
-    The issue with using a binary tree for this problem is finding the right way to represent decisions. In the 0/    1 knapsack problem each decision to add or ignore an item was a boolean choice. 
+    The issue with using a binary tree for this problem is finding the right way to represent decisions. 
+    In the 0/    1 knapsack problem each decision to add or ignore an item was a boolean choice. 
 
     What if we take a step back and consider more generally applying a graph to this problem. What would it look
     like? Well we could begin with a root node. The root node has: egg weights, weight to achieve. Our graph
-    G = (V, E) has Vertices with information and Edges represent the associated cost (in weight) of following a       path. If the path costs more than the desired weight then that path is not chosen.
+    G = (V, E) has Vertices with information and Edges represent the associated cost (in weight) of following a 
+    path. If the path costs more than the desired weight then that path is not chosen.
 
     Or do you start at the root node and then divide by the egg weight and choose the path with the highest
     number? Doing it this way means you go down to the bottom left of the graph and this represents taking
@@ -52,6 +56,7 @@ def dp_make_weight(egg_weights, target_weight, memo = {}):
     We will implement a recursive function which computes combinations of eggs that equal the provided 
     weight then return the set with least length. We will use a dictionary to store memos to avoid unecessary 
     recomputations. 
+    """
     pass
 
 # EXAMPLE TESTING CODE, feel free to add more if you'd like
