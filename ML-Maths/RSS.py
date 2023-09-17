9b92b97c16116207a9a1f2f79b94419d33449ff6
lochie <disrute@gmail.com>
2023-09-17 19:49:17 +1000

    trying rss

---
+++ ML-Maths/RSS.py
@@ -0,0 +1,12 @@
+from sklearn.linear_model import LinearRegression
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+model = LinearRegression()
+
+model.fit(X, y)
+
+y_prediction = model.predict(X)
+
+rss = sum((y - y_prediction)**2)
