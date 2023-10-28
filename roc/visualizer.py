78bb10e1e3db29294a1860b56064bd61b2edd8e0
Loch <disrute@gmail.com>
2023-10-28 12:09:05 +1100

    add plots

---
+++ roc/visualizer.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import numpy as np
+from matplotlib.pyplot import subplots
+import matplotlib.dates as mdates
+import datetime as dt
+
+GRAPH_PATH = "/graphs"
+
+ds = pd.read_csv('data/dandenong.csv', index_col='id')
+
+fig, ax = subplots(figsize=(8, 8))
+#ax.plot(ds['PM10'], ds['NO2'], 'o')
+#fig.savefig(f'{GRAPH_PATH}/1')
+
+#print(ds['NO2'])
+
+
+# We must construct a list of the datetimes. Let's use this method:
+# Sample datetime: 2022-01-01 00:00:00
+dates = [date for date in ds['datetime_local']]
+x = [dt.datetime.strptime(d, '%Y-%m-%d %H:%M:%S').date() for d in dates]
+# assuming it's 1:1 we plot PM10 on y-axis:
+y = ds['PM10']
+print(x)
+
+fig.gca().xaxis.set_major_formatter(mdates.DateFormatter('%d/%m'))
+fig.gca().xaxis.set_major_locator(mdates.MonthLocator())
+#ax.locator_params(axis='x', nbins=12)
+ax.plot(x, y, 'o')
+fig.savefig('newplt2')
+
+#print(dates)
+
+# Todo: take average PM10 for each day then plot average PM10 p/day vs day.
