671e83dde0a4cec59b2b3c21c8e0f519f776bf31
Loch <disrute@gmail.com>
2023-11-10 12:22:20 +1100

    compl chp1

--- islp/applied/chp1.1.py
+++ islp/applied/chp1.1.py
@@ -55,5 +55,21 @@ Auto.plot.scatter(x='horsepower', y='mpg')
 Auto.boxplot(column='acceleration', by='cylinders')
 # Above describes how the acceleration of the vehicles with varying cylinder sizes
 # is distributed 
+plt.show()
 
+# (f) Suppose we wish to predict mpg, which other variables might be useful
+# in predicting mpg?
+
+# Let's plot some mpg versus x graphs.
+figure, axis = plt.subplots(1, 2)
+axis[0].scatter(Auto['year'], Auto['mpg'])
+axis[0].set_title("mpg vs year")
+# There is a weak positive linear correlation between year and mpg
+# suggesting year can be used to predict mpg, higher year => higher mpg.
+# From earlier graphs we know there is a weak neg correlation between
+# horsepower and mpg which may be used to predict mpg values.
+axis[1].scatter(Auto['weight'], Auto['mpg'])
+axis[1].set_title("mpg vs weight")
+# There is a strong negative linear correlation between weight and mpg,
+# we may predict mpg from the weight of the vehicle.
 plt.show()
